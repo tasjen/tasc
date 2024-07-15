c1a34834b8e02b18139c9a3ab66958261fd16654
tasjen <129994991+tasjen@users.noreply.github.com>
2024-07-15 07:05:39 +0700

    Change build filename to server.js #skip

--- server/esbuild-script.js
+++ server/esbuild-script.js
@@ -2,7 +2,7 @@ const { build } = require('esbuild');
 
 build({
   entryPoints: ['./tsc_build/index.js'],
-  outfile: './build/index.js',
+  outfile: './build/server.js',
   platform: 'node',
   bundle: true,
   minify: true,
