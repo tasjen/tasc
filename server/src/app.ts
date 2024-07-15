0a0bab42e80908159ca0bfeccf8aa005cc7d25c5
tasjen <129994991+tasjen@users.noreply.github.com>
2024-07-15 07:41:48 +0700

    Remove NODE_ENV check for static middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -26,12 +26,10 @@ app.use(cors());
 app.use(express.json());
 app.use(logger());
 
-if (process.env.NODE_ENV === 'prod') {
-  app.use(express.static(path.join(__dirname, 'dist')));
-  app.get('/*', (_, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
-  });
-}
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('/*', (_, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
 app.use('/api/users', userRouter);
 app.use('/api/projects', projectRouter);
