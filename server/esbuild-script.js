37b816f5e741eeb30fc96dbb1d1d3dece2c5ccdb
tasjen <129994991+tasjen@users.noreply.github.com>
2024-07-15 07:18:54 +0700

    Add IS_VERCEL env for vercel deployment #skip

--- server/esbuild-script.js
+++ server/esbuild-script.js
@@ -2,7 +2,7 @@ const { build } = require('esbuild');
 
 build({
   entryPoints: ['./tsc_build/index.js'],
-  outfile: './build/server.js',
+  outfile: `./build/${process.env.IS_VERCEL ? 'index' : 'server'}.js`,
   platform: 'node',
   bundle: true,
   minify: true,
