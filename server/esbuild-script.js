a76f0aebe0a207a8257750203d7f85bf213232af
tasjen <129994991+tasjen@users.noreply.github.com>
2024-07-19 02:02:17 +0700

    Give up on Vercel deployment #skip

--- server/esbuild-script.js
+++ server/esbuild-script.js
@@ -2,10 +2,10 @@ const { build } = require('esbuild');
 
 build({
   entryPoints: ['./tsc_build/index.js'],
-  outfile: `./build/${process.env.IS_VERCEL ? 'index' : 'server'}.js`,
+  outfile: `./dist/index.js`,
   platform: 'node',
   bundle: true,
   minify: true,
-  sourcemap: true,
+  // sourcemap: true,
   sourcesContent: false,
 });
